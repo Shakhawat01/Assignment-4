d5d76b025ff9d243fc33f2024c192a93f31913b5
Shakhawat01 <mdshakhawathossensakib01@gmail.com>
2026-03-18 07:02:56 +0600

    create toggleStyle function for switch tab

--- script/app.js
+++ script/app.js
@@ -22,3 +22,30 @@ function calculateTotal() {
   rejectedCount.innerText = rejectedList.length;
 }
 calculateTotal();
+function toggleStyle(id) {
+  allFilterBtn.classList.remove("btn-primary");
+  interviewFilterBtn.classList.remove("btn-primary");
+  rejectedFilterBtn.classList.remove("btn-primary");
+
+  allFilterBtn.classList.add("bg-base-200");
+  interviewFilterBtn.classList.add("bg-base-200");
+  rejectedFilterBtn.classList.add("bg-base-200");
+
+  const selected = document.getElementById(id);
+  selected.classList.remove("bg-base-200");
+  selected.classList.add("btn-primary");
+  currentStatus = id;
+
+  if (id === "interview-filter-btn") {
+    allCardsSection.classList.add("hidden");
+    filteredSection.classList.remove("hidden");
+    renderInterviews();
+  } else if (id === "all-filter-btn") {
+    allCardsSection.classList.remove("hidden");
+    filteredSection.classList.add("hidden");
+  } else if (id === "rejected-filter-btn") {
+    allCardsSection.classList.add("hidden");
+    filteredSection.classList.remove("hidden");
+    renderRejected();
+  }
+}
